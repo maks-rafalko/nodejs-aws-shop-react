c030d77f177485d29c78ac6650986b81fcf8fd80
Rafalko, Maksim <maks.rafalko@gmail.com>
2023-05-24 14:23:46 +0300

    refactor: add S3 bucket by CDK and add URL to output. For now - site is uploaded and served publicly from S3

--- cdk/src/main/app/shop-stack.ts
+++ cdk/src/main/app/shop-stack.ts
@@ -1,15 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
+import * as S3Deployment from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from "path";
 
 export class ShopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'ShopStaticSiteServeBucket', {
+    const bucket = new s3.Bucket(this, 'ShopStaticSiteServeBucket', {
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      // TODO remove it after cloudfront setup
+      publicReadAccess: true,
+    });
+
+    new S3Deployment.BucketDeployment(this, 'ShopStaticSiteServeBucketDeployment', {
+      destinationBucket: bucket,
+      sources: [S3Deployment.Source.asset(path.resolve(__dirname, '..', '..', '..', '..', 'dist'))],
+    });
+
+    new cdk.CfnOutput(this, 'bucketUrl', {
+      value: bucket.bucketWebsiteUrl,
+      description: 'The URL of the bucket',
+      exportName: 'ShopStaticSiteServeBucketURL',
     });
   }
 }
